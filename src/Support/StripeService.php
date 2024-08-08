39e69699ae4d99f025993ef884dd5d61c2b4400c
Duncan <74963497+dnrdigital@users.noreply.github.com>
2024-08-08 21:41:53 +0100

    Update StripeService.php - initial pseudo code

--- src/Support/StripeService.php
+++ src/Support/StripeService.php
@@ -436,4 +436,32 @@ class StripeService
                 ->values();
         });
     }
+
+    public function fulfillCheckout($session_id) {
+        
+        // Set your secret key. Remember to switch to your live secret key in production.
+        
+        // TODO: Log the string "Fulfilling Checkout Session $session_id"
+        // TODO: Make this function safe to run multiple times,
+        // even concurrently, with the same session ID
+        // TODO: Make sure fulfillment hasn't already been
+        // peformed for this Checkout Session
+        
+        // Retrieve the Checkout Session from the API with line_items expanded
+        
+        $checkout_session = $stripe->checkout->sessions->retrieve($session_id, [
+            'expand' => ['line_items'],
+          ]);
+        
+        // Check the Checkout Session's payment_status property
+        // to determine if fulfillment should be peformed
+        
+        if ($checkout_session->payment_status != 'unpaid') {
+            
+            // TODO: Perform fulfillment of the line items
+            // TODO: Record/save fulfillment status for this
+            // Checkout Session
+        
+        }
+    }
 }
