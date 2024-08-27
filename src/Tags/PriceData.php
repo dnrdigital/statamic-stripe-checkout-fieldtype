6713383758b492f0b4cffb0bc9b119b0e7754623
dnrdigital <duncan@dnr.pm>
2024-08-27 11:29:20 +0100

    Pass back products

--- src/Tags/PriceData.php
+++ src/Tags/PriceData.php
@@ -21,11 +21,8 @@ class PriceData extends \Statamic\Tags\Tags
     {
         // get products from the stripe service
         $products = app(StripeService::class)->getProducts();
-        $test = 'hello';
 
-
-        return $test;
-        //return $products;
+        return $products;
     }
 
 }
