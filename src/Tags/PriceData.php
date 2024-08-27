7af2255f568b7ac4e14a9ae4ecbb92b045568c74
dnrdigital <duncan@dnr.pm>
2024-08-27 21:24:48 +0100

    Tag to return name and amount by id

--- src/Tags/PriceData.php
+++ src/Tags/PriceData.php
@@ -7,22 +7,46 @@ use MityDigital\StatamicStripeCheckoutFieldtype\Support\StripeService;
 class PriceData extends \Statamic\Tags\Tags
 {
 
-    public function name()
-    {
-        $this->params->get('id');
-    }
+   public function name()
+   {
+       $prices = $this->all();
+       return $prices[$this->params->get('id')]['name'];
+   }
 
-    public function amount()
-    {
-        $this->params->get('id');
-    }
+   public function amount()
+   {
+       $prices = $this->all();
+       return $prices[$this->params->get('id')]['amount'];
+   }
 
-    public function all()
-    {
-        // get products from the stripe service
-        $products = app(StripeService::class)->getProducts();
+   public function all()
+   {
+       // get products from the stripe service
+       $products = app(StripeService::class)->getProducts();
 
-        return json_encode($products);
-    }
+       $prices = $products->map(fn ($product) => $product['prices']->map(function ($price) use ($product) {
+           // get the name
+           $name = $price['name'];
+           $amount = $price['amount'];
+
+           // set the name string
+           if ($name) {
+               $name = $name;
+           } else {
+               $name = $product['name'];
+           }
+
+           return ['id' => $price['id'], 'name' => $name, 'amount' => $amount ];
+       }))
+       ->flatten(1)
+       ->mapWithKeys(fn ($price) => [
+               $price['id'] => [
+                   'name' => $price['name'],
+                   'amount' => $price['amount']
+               ]
+       ]);
+
+       return $prices;
+   }
 
 }
